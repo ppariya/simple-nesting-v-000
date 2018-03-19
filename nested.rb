55e0ea428bb94e20ca76dcb513a9edfe6ad142a3
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 17:31:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	puts	"#{programmer_hash[:dennis_ritchie][:languages]}"
+	languages = "#{{programmer_hash[:dennis_ritchie][:languages]}"
+	languages
 end
 
 def adding_matz
