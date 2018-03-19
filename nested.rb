1ac7a6ceb0654cfce0f473d507aecbe43e285099
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 17:53:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+		 return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,4 +125,5 @@ def adding_to_dennis
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		  return programmer_hash
 end
